21d95b0b414e0713348191d20f05e9fea7a05965
Yury Semenov <yusemru@gmail.com>
2022-02-09 21:00:22 +0300

    added simple logic constraints and sat example

---
+++ examples/sat/src/main.rs
@@ -0,0 +1,101 @@
+/* This program solves SAT problem.
+ *
+ * Single command line argument:
+ * path to file in DIMACS CNF format (https://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/satformat.ps)
+ *
+ * Use sample_satisfiable.cnf and sample_unsatisfiable.cnf for example (files taken from SATLIB: https://www.cs.ubc.ca/~hoos/SATLIB/benchm.html)
+ */
+use ezcp::logic::{AndConstraint, NegateConstraint, OrConstraint};
+use ezcp::solver::Solver;
+use ezcp::value_selector::MinValueSelector;
+use ezcp::variable_selector::FirstFailVariableSelector;
+use std::boxed::Box;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_cnf_file(filename: &str) -> (usize, Vec<Vec<(usize, bool)>>) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut n_vars = 0;
+    let mut n_clauses = 0;
+    let mut clauses = vec![Vec::new(); 1];
+    for line in reader.lines().map(|l| l.unwrap()) {
+        if line.is_empty() {
+            continue;
+        }
+        let begin = line.as_bytes()[0] as char;
+        if begin == 'c' {
+            continue;
+        }
+        if begin == 'p' {
+            let tokens: Vec<_> = line.split(' ').filter(|x| !x.is_empty()).collect();
+            assert!(tokens.len() == 4);
+            if tokens[1] != "cnf" {
+                panic!("Unsupported format: {}", tokens[1]);
+            }
+            n_vars = tokens[2].parse::<usize>().unwrap();
+            n_clauses = tokens[3].parse::<usize>().unwrap();
+            continue;
+        }
+        if !begin.is_ascii_digit() && begin != ' ' && begin != '-' {
+            continue;
+        }
+        for literal in line.split(' ').filter(|x| !x.is_empty()).map(|x| x.parse::<i32>().unwrap()) {
+            if literal == 0 {
+                clauses.push(Vec::new());
+            } else {
+                if literal < 0 {
+                    clauses.last_mut().unwrap().push(((-literal - 1) as usize, false));
+                } else {
+                    clauses.last_mut().unwrap().push(((literal - 1) as usize, true));
+                }
+            }
+        }
+    }
+    clauses = clauses.iter().filter(|x| !x.is_empty()).cloned().collect();
+    assert!(clauses.len() == n_clauses);
+    (n_vars, clauses)
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let (n_vars, clauses) = read_cnf_file(&args[1]);
+    let mut solver = Solver::new(
+        Box::new(FirstFailVariableSelector {}),
+        Box::new(MinValueSelector {}),
+    );
+    let mut vars = Vec::with_capacity(n_vars);
+    let mut negations = Vec::with_capacity(n_vars);
+    let mut clause_vars = Vec::with_capacity(clauses.len());
+    for i in 0..n_vars {
+        let v = solver.new_variable(0, 1, format!("v_{}", i));
+        let nv = solver.new_variable(0, 1, format!("not v_{}", i));
+        vars.push(v.clone());
+        negations.push(nv.clone());
+        solver.add_constraint(Box::new(NegateConstraint::new(v, nv)));
+    }
+    for i in 0..clauses.len() {
+        let cv = solver.new_variable(0, 1, format!("clause_{}", i));
+        clause_vars.push(cv.clone());
+        let mut v = Vec::new();
+        for (id, neg) in clauses[i].iter().cloned() {
+            if neg {
+                v.push(negations[id].clone());
+            } else {
+                v.push(vars[id].clone());
+            }
+        }
+        solver.add_constraint(Box::new(OrConstraint::new(cv, v)));
+    }
+    let sat_var = solver.new_variable(1, 1, format!("sat"));
+    solver.add_constraint(Box::new(AndConstraint::new(sat_var.clone(), clause_vars.clone())));
+    if !solver.solve() {
+        println!("Unsatisfiable.");
+    } else {
+        println!("Satisfiable.");
+        for v in &vars {
+            print!("{} ", v.borrow().value());
+        }
+        println!("");
+    }
+}
---
+++ src/logic.rs
@@ -0,0 +1,352 @@
+use crate::constraint::Constraint;
+use crate::events::Event;
+use crate::propagator::{Propagator, PropagatorControlBlock};
+use crate::solver::Solver;
+use crate::variable::Variable;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// result = vars[0] AND vars[1] AND ... AND vars[vars.len() - 1]
+pub struct AndConstraint {
+    result: Rc<RefCell<Variable>>,
+    vars: Vec<Rc<RefCell<Variable>>>,
+}
+
+impl AndConstraint {
+    pub fn new(result: Rc<RefCell<Variable>>, vars: Vec<Rc<RefCell<Variable>>>) -> Self {
+        Self { result, vars }
+    }
+}
+
+impl Constraint for AndConstraint {
+    fn satisfied(&self) -> bool {
+        let result = match self.result.borrow().is_assigned() {
+            true => self.result.borrow().value(),
+            false => {
+                return false;
+            }
+        };
+        for v in &self.vars {
+            if !v.borrow().is_assigned() {
+                return false;
+            }
+            if v.borrow().value() == 0 {
+                return result == 0;
+            }
+        }
+        result != 0
+    }
+
+    fn create_propagators(&self, solver: &mut Solver) {
+        let p = Rc::new(RefCell::new(AndPropagator::new(
+            self.result.clone(),
+            self.vars.clone(),
+            solver.new_propagator_id(),
+        )));
+        solver.add_propagator(p.clone());
+        p.borrow().listen(p.clone());
+    }
+}
+
+pub struct AndPropagator {
+    pcb: PropagatorControlBlock,
+    result: Rc<RefCell<Variable>>,
+    vars: Vec<Rc<RefCell<Variable>>>,
+}
+
+impl AndPropagator {
+    pub fn new(result: Rc<RefCell<Variable>>, vars: Vec<Rc<RefCell<Variable>>>, id: usize) -> Self {
+        Self {
+            pcb: PropagatorControlBlock::new(id),
+            result,
+            vars,
+        }
+    }
+}
+
+impl Propagator for AndPropagator {
+    fn listen(&self, self_pointer: Rc<RefCell<dyn Propagator>>) {
+        self.result
+            .borrow_mut()
+            .add_listener(self_pointer.clone(), Event::Modified);
+        for v in &self.vars {
+            v.borrow_mut()
+                .add_listener(self_pointer.clone(), Event::Modified);
+        }
+    }
+
+    fn propagate(&mut self) {
+        if self.result.borrow().is_assigned() {
+            let result = self.result.borrow().value();
+            if result == 1 {
+                for v in &self.vars {
+                    v.borrow_mut().assign(1);
+                }
+            } else {
+                let mut ones = 0;
+                let mut unknown = 0;
+                for v in &self.vars {
+                    if v.borrow().is_assigned() {
+                        if v.borrow().value() == 1 {
+                            ones += 1;
+                        }
+                    } else {
+                        unknown += 1;
+                    }
+                }
+                if ones == self.vars.len() {
+                    self.result.borrow_mut().fail();
+                } else {
+                    if unknown == 1 && 1 + ones == self.vars.len() {
+                        for v in &self.vars {
+                            if !v.borrow().is_assigned() {
+                                v.borrow_mut().assign(0);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            let mut can0 = false;
+            let mut can1 = true;
+            for v in &self.vars {
+                if v.borrow().possible(0) {
+                    can0 = true;
+                }
+                if !v.borrow().possible(1) {
+                    can1 = false;
+                }
+            }
+            if !can0 {
+                self.result.borrow_mut().remove(0);
+            }
+            if !can1 {
+                self.result.borrow_mut().remove(1);
+            }
+        }
+    }
+
+    fn get_cb(&self) -> &PropagatorControlBlock {
+        &self.pcb
+    }
+
+    fn get_cb_mut(&mut self) -> &mut PropagatorControlBlock {
+        &mut self.pcb
+    }
+
+    fn is_idemponent(&self) -> bool {
+        true
+    }
+}
+
+// result = vars[0] OR vars[1] OR ... OR vars[vars.len() - 1]
+pub struct OrConstraint {
+    result: Rc<RefCell<Variable>>,
+    vars: Vec<Rc<RefCell<Variable>>>,
+}
+
+impl OrConstraint {
+    pub fn new(result: Rc<RefCell<Variable>>, vars: Vec<Rc<RefCell<Variable>>>) -> Self {
+        Self { result, vars }
+    }
+}
+
+impl Constraint for OrConstraint {
+    fn satisfied(&self) -> bool {
+        let result = match self.result.borrow().is_assigned() {
+            true => self.result.borrow().value(),
+            false => {
+                return false;
+            }
+        };
+        for v in &self.vars {
+            if !v.borrow().is_assigned() {
+                return false;
+            }
+            if v.borrow().value() == 1 {
+                return result != 0;
+            }
+        }
+        result == 0
+    }
+
+    fn create_propagators(&self, solver: &mut Solver) {
+        let p = Rc::new(RefCell::new(OrPropagator::new(
+            self.result.clone(),
+            self.vars.clone(),
+            solver.new_propagator_id(),
+        )));
+        solver.add_propagator(p.clone());
+        p.borrow().listen(p.clone());
+    }
+}
+
+pub struct OrPropagator {
+    pcb: PropagatorControlBlock,
+    result: Rc<RefCell<Variable>>,
+    vars: Vec<Rc<RefCell<Variable>>>,
+}
+
+impl OrPropagator {
+    pub fn new(result: Rc<RefCell<Variable>>, vars: Vec<Rc<RefCell<Variable>>>, id: usize) -> Self {
+        Self {
+            pcb: PropagatorControlBlock::new(id),
+            result,
+            vars,
+        }
+    }
+}
+
+impl Propagator for OrPropagator {
+    fn listen(&self, self_pointer: Rc<RefCell<dyn Propagator>>) {
+        self.result
+            .borrow_mut()
+            .add_listener(self_pointer.clone(), Event::Modified);
+        for v in &self.vars {
+            v.borrow_mut()
+                .add_listener(self_pointer.clone(), Event::Modified);
+        }
+    }
+
+    fn propagate(&mut self) {
+        if self.result.borrow().is_assigned() {
+            let result = self.result.borrow().value();
+            if result == 1 {
+                let mut ones = 0;
+                for v in &self.vars {
+                    if v.borrow().possible(1) {
+                        ones += 1;
+                    }
+                }
+                if ones == 0 {
+                    self.result.borrow_mut().fail();
+                    return;
+                }
+                if ones == 1 {
+                    for v in &self.vars {
+                        if v.borrow().possible(1) {
+                            v.borrow_mut().assign(1);
+                        }
+                    }
+                }
+            } else {
+                for v in &self.vars {
+                    v.borrow_mut().assign(0);
+                }
+            }
+        } else {
+            let mut can1 = false;
+            let mut can0 = true;
+            for v in &self.vars {
+                if v.borrow().possible(1) {
+                    can1 = true;
+                }
+                if !v.borrow().possible(0) {
+                    can0 = false;
+                }
+            }
+            if !can0 {
+                self.result.borrow_mut().remove(0);
+            }
+            if !can1 {
+                self.result.borrow_mut().remove(1);
+            }
+        }
+    }
+
+    fn get_cb(&self) -> &PropagatorControlBlock {
+        &self.pcb
+    }
+
+    fn get_cb_mut(&mut self) -> &mut PropagatorControlBlock {
+        &mut self.pcb
+    }
+
+    fn is_idemponent(&self) -> bool {
+        true
+    }
+}
+
+// x = not y
+pub struct NegateConstraint {
+    x: Rc<RefCell<Variable>>,
+    y: Rc<RefCell<Variable>>,
+}
+
+impl NegateConstraint {
+    pub fn new(x: Rc<RefCell<Variable>>, y: Rc<RefCell<Variable>>) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Constraint for NegateConstraint {
+    fn satisfied(&self) -> bool {
+        if !self.x.borrow().is_assigned() || !self.y.borrow().is_assigned() {
+            false
+        } else {
+            self.x.borrow().value() != self.y.borrow().value()
+        }
+    }
+
+    fn create_propagators(&self, solver: &mut Solver) {
+        let p = Rc::new(RefCell::new(NegatePropagator::new(
+            self.x.clone(),
+            self.y.clone(),
+            solver.new_propagator_id(),
+        )));
+        solver.add_propagator(p.clone());
+        p.borrow().listen(p.clone());
+    }
+}
+
+pub struct NegatePropagator {
+    pcb: PropagatorControlBlock,
+    x: Rc<RefCell<Variable>>,
+    y: Rc<RefCell<Variable>>,
+}
+
+impl NegatePropagator {
+    pub fn new(x: Rc<RefCell<Variable>>, y: Rc<RefCell<Variable>>, id: usize) -> Self {
+        Self {
+            pcb: PropagatorControlBlock::new(id),
+            x,
+            y,
+        }
+    }
+}
+
+impl Propagator for NegatePropagator {
+    fn listen(&self, self_pointer: Rc<RefCell<dyn Propagator>>) {
+        self.x
+            .borrow_mut()
+            .add_listener(self_pointer.clone(), Event::Modified);
+        self.y
+            .borrow_mut()
+            .add_listener(self_pointer.clone(), Event::Modified);
+    }
+
+    fn propagate(&mut self) {
+        for val in 0..2 {
+            if !self.x.borrow().possible(val) {
+                self.y.borrow_mut().remove(val ^ 1);
+            }
+        }
+        for val in 0..2 {
+            if !self.y.borrow().possible(val) {
+                self.x.borrow_mut().remove(val ^ 1);
+            }
+        }
+    }
+
+    fn get_cb(&self) -> &PropagatorControlBlock {
+        &self.pcb
+    }
+
+    fn get_cb_mut(&mut self) -> &mut PropagatorControlBlock {
+        &mut self.pcb
+    }
+
+    fn is_idemponent(&self) -> bool {
+        true
+    }
+}
